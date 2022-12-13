37b59ba73a103c7d42e2c87fbfc326217a0962b4
Marek Frańczak <marekfranczak1@gmail.com>
2022-12-13 10:28:52 +0100

    Prepared project for migration to MySQL and SpringBoot. Changed pom.xml and added new project structure.

---
+++ src/main/java/garden/database/GardenDataBaseApplication.java
@@ -0,0 +1,13 @@
+package garden.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenDataBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/garden/database/GardenDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package garden.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenDataBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
